15dedff8f93ccdc7fa787235d4710c12e2ccb28c
harsh dhobe <harshdhobe123@gmail.com>
2024-09-13 01:09:45 +0530

    modified function

--- components/Buymecoffee.tsx
+++ components/Buymecoffee.tsx
@@ -103,7 +103,7 @@ export const Buymecoffee = () => {
               setMessage(""); // Reset message
             }}
           >
-            Buy Coffee
+            Buy Me Coffee
           </TransactionButton>
         </div>
       </div>
